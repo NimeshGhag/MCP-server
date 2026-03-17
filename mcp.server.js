ec8c1b35fe04d578de0a4191762e14c13fc2ce20
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-17 12:04:16 +0530

    feat: add CalculateBMI tool to MCP server

--- mcp.server.js
+++ mcp.server.js
@@ -7,10 +7,42 @@ const server = new McpServer({
   version: "1.0.0",
 });
 
-const transport = new StdioServerTransport();
-try {
-  await server.connect(transport);
-  console.log("Mcp server connected");
-} catch (err) {
-  console.error(err);
+server.registerTool(
+  "CalculateBMI",
+  {
+    title: "CalculateBMI",
+    description: "Calculate BMI based on weight and height",
+    inputSchema: {
+      weightKg: z.number().describe("Weight in kilograms"),
+      heightM: z.number().describe("Height in meters"),
+    },
+    outputSchema: { bmi: z.number().describe("Calculated BMI") },
+  },
+  async ({ weightKg, heightM }) => {
+    const bmi = weightKg / (heightM * heightM);
+    return {
+      content: [
+        {
+          type: "text",
+          text: `Your BMI is ${bmi.toFixed(2)}`,
+        },
+      ],
+      structuredContent: {
+        bmi: Number(bmi.toFixed(2)),
+      },
+    };
+  },
+);
+
+async function startServer() {
+  const transport = new StdioServerTransport();
+
+  try {
+    await server.connect(transport);
+    console.log("MCP server connected");
+  } catch (err) {
+    console.error("Error starting MCP server:", err);
+  }
 }
+
+startServer();
