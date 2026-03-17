bdaa1917c4757285e825b7cb0f56b763ff45d6a9
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-17 12:08:37 +0530

    feat: implement MCP client connection logic

---
+++ mcp.client.js
@@ -0,0 +1,21 @@
+import { Client } from "@modelcontextprotocol/sdk/client";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+const transport = new StdioClientTransport({
+  command: "node",
+  args: ["mcp.server.js"],
+});
+const client = new Client({
+  name: "my-client",
+  version: "1.0.0",
+});
+
+async function startClient() {
+  try {
+    await client.connect(transport);
+    console.log("MCP client connected");
+  } catch (err) {
+    console.error("Error starting MCP client:", err);
+  }
+}
+startClient();
